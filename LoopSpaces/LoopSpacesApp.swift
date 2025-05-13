819039ab701f991fa16de33ada2deb7b8dfd1be6
Pallepadehat <patrick.jakobsen@icloud.com>
2025-05-13 10:22:50 +0200

    Initial Commit

---
+++ LoopSpaces/LoopSpacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopSpacesApp.swift
+//  LoopSpaces
+//
+//  Created by Patrick Jakobsen on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoopSpacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
